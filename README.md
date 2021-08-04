3e023d9f9e5fb4692282d0e2ef6f6eb6d6b8149b
Justin Black Chou <zhou.jiatai@foxmail.com>
2021-08-04 10:44:16 +0800

    Update

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # ZJtRand 随机数生成器
+
 ## 编译
+
 ### Windows
 
 执行：
 
     git clone https://github.com/ZhouJiatai/ZJtRand.git
     cd ZJtRand
-    mingw32-make
+    g++ main.cpp -o ZJtRand.exe
 
-可执行文件将会位于 `bin\Release\ZJtRand.exe`
+可执行文件将会位于 `.\ZJtRand.exe`
 
-## 许可证
+***注意：这样生成的可执行文件将会依赖 `libgcc_s_dw2-1.dll` `libwinpthread-1.dll` `libstdc++-6.dll`。如要抗拒这种依赖，请执行：***
+
+    git clone https://github.com/ZhouJiatai/ZJtRand.git
+    cd ZJtRand
+    g++ main.cpp -static -o ZJtRand.exe
+
+### GNU/Linux
+
+执行：
+
+    git clone https://github.com/ZhouJiatai/ZJtRand.git
+    cd ZJtRand
+    g++ main.cpp -o ZJtRand
+
+可执行文件将会位于 `./ZJtRand`
+
+***注意：这样生成的可执行文件依赖关系本人还不知道。如果有人知道了，请告诉我！***
+
+## 版权
 
 Copyright (C) 2021 Zhou Jiatai  
-License: [GPL-3.0-only](https://github.com/ZhouJiatai/ZJtRand/blob/main/COPYING.txt)
+许可证: [GPL-3.0-or-later](https://github.com/ZhouJiatai/ZJtRand/blob/main/COPYING.txt) (GPLv3+)
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 /*
-    ZJtRand 0.2.0 Alpha
-
     Copyright (C) 2021 Zhou Jiatai
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, version 3.
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +16,9 @@
 */
 
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
+#include <string>
+#include <sstream>
 using namespace std;
 
 #if (__cplusplus < 201103L)
@@ -27,9 +27,9 @@ using namespace std;
 
 inline void beginning()
 {
-    cout << "ZJtRand 随机数生成器 0.2.0 Alpha\n"
+    cout << "ZJtRand 随机数生成器 0.2.0\n"
          << "Copyright (C) 2021 Zhou Jiatai\n\n"
-         << "本程序为自由软件，在自由软件联盟发布的GNU通用公共许可协议的约束下，你可以对其进行再发布及修改。协议版本为第三版。\n"
+         << "本程序为自由软件，在自由软件联盟发布的GNU通用公共许可协议的约束下，你可以对其进行再发布及修改。协议版本为第三版或（随你）更新的版本。\n"
          << "我们希望发布的这款程序有用，但不保证，甚至不保证它有经济价值和适合特定用途。详情参见GNU通用公共许可协议。\n"
          << "你理当已收到一份GNU通用公共许可协议的副本，如果没有，请查阅<http://www.gnu.org/licenses/>\n\n"
          << "请注意：本程序产生的随机数位于"
@@ -41,11 +41,23 @@ inline void srand_init()
     srand(time(nullptr));
 }
 
+inline int getint()
+{
+    string s;
+    stringstream ss;
+    int i = 0;
+    getline(cin, s);
+    ss << s;
+    ss >> i;
+    return i;
+}
+
 inline bool getrand()
 {
     int maxn;
     cout << "\n请输入要产生的随机数的最大值（小于32769，大于0，输入0表示退出）：";
-    cin >> maxn;
+    maxn = getint();
+    cout << "您输入的数是：" << maxn << '\n';
     if(maxn == 0)
         return false;
     else if(maxn >= 32769)
@@ -64,8 +76,7 @@ inline bool getrand()
 
 inline void pause()
 {
-    cout << "\n请按回车键继续...";
-    cin.get();
+    cout << "\n请按回车键退出...";
     cin.get();
 }
 
